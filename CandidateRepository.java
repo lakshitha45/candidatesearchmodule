9230c5481549913a4bccc58af261ead60d6f9912
lakshitha45 <lakshitha465200@gmail.com>
2026-04-04 11:08:24 +0530

    Add files via upload

---
+++ CandidateRepository.java
@@ -0,0 +1,15 @@
+package com.jobportal.backend.repository;
+
+import com.jobportal.backend.model.Candidate;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface CandidateRepository
+        extends MongoRepository<Candidate, String>, CandidateRepositoryCustom {
+
+    @Query(value = "{ 'location': ?0, 'locality': ?1 }")
+    List<Candidate> findByLocationAndLocality(String location, String locality);
+
+}
